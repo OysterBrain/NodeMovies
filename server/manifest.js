b8b805d1d66649899ccffb073aa8ef91283d8f9b
Devin Ivy <devin@bigroomstudios.com>
2019-10-08 00:20:55 -0400

    objection

--- server/manifest.js
+++ server/manifest.js
@@ -34,7 +34,28 @@ module.exports = new Confidence.Store({
                 options: {}
             },
             {
+<<<<<<< HEAD
                 plugin: './plugins/swagger'
+=======
+                plugin: 'schwifty',
+                options: {
+                    $filter: 'NODE_ENV',
+                    $default: {},
+                    $base: {
+                        migrateOnStart: true,
+                        knex: {
+                            client: 'sqlite3',
+                            useNullAsDefault: true,     // Suggested for sqlite3
+                            connection: {
+                                filename: ':memory:'
+                            }
+                        }
+                    },
+                    production: {
+                        migrateOnStart: false
+                    }
+                }
+>>>>>>> fc1df67... (flavor) objection v2.5.1
             },
             {
                 plugin: {
